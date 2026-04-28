25b4221b4077584410fa765cd39e9ac5e9c3647e
Onix <piyvoda20033@gmail.com>
2026-04-28 17:39:39 +0300

    minor fixes due hw

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -47,5 +47,5 @@ export const updateNote = async (req, res) => {
   );
   if (!note) throw createHttpError(404, 'Note not found');
 
-  res.status(201).json(note);
+  res.status(200).json(note);
 };
--- src/server.js
+++ src/server.js
@@ -22,5 +22,5 @@ app.use(errorHandler);
 await connectMongoDB();
 
 app.listen(PORT, () => {
-  console.log('Server is running');
+  console.log(`Server is running at port ${PORT}`);
 });
